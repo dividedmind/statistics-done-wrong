31eed621862bcf7030f73817718695f3737d4356
Alex Reinhart <alex@refsmmat.com>
2013-09-25 10:37:50 -0500

    R: fix inflation plot shading

--- R/inflation.R
+++ R/inflation.R
@@ -1,8 +1,10 @@
 samples = rnorm(5000, mean=1)
 
+breaks = seq(-3, 5, 0.2)
+
 png('../plots/inflation.png', 
     width=600, height=400, pointsize=16)
-hist(samples, breaks=40, freq=FALSE, col=c(rep(0,30), rep("gray73", 10)),
+hist(samples, breaks=breaks, freq=FALSE, col=c(rep(0,30), rep("gray73", 10)),
      main="", xlab="Observed effect size", ylab="Probability", las=1)
 abline(v=3, lty=2)
 dev.off()
@@ -11,8 +13,7 @@ library(extrafont)
 
 cairo_pdf('../plots/inflation.pdf', 
           width=4, height=3, family="Linux Biolinum O", pointsize = 10)
-hist(samples, breaks=40, freq=FALSE, col=c(rep(0,30), rep("gray73", 10)),
+hist(samples, breaks=breaks, freq=FALSE, col=c(rep(0,30), rep("gray73", 10)),
      main="", xlab="Observed effect size", ylab="Probability", las=1)
 abline(v=3, lty=2)
 dev.off()
-
